bf0750cb4c31d22e14e2bc9a2387b6bad48f0e2d
Kinan T <kinan.t@gmail.com>
2015-02-28 18:06:48 -0600

    Load enemy car resources.

--- js/engine.js
+++ js/engine.js
@@ -217,7 +217,8 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/enemy-car.png'
     ]);
     Resources.onReady(init);
 
